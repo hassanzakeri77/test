c7d2791339e2739350f6bec784ff2ebca8e3b50e
Hassan_Zakeri <hassan.1377@gmail.com>
2021-01-31 01:02:34 +0330

    Revert "Revert "added 1 and 2""
    This reverts commit 7fe3550d1963d8d98317fa0540411e507e13b11c.

---
+++ 2.py
@@ -0,0 +1,26 @@
+#! /bin/env python3
+import os
+import sys
+import time
+
+def like_cheese():
+    var = input("Hi! I like cheese! Do you like cheese?").lower()
+    if var == "yes":
+        print("That's awesome!")
+
+while True:
+    pass
+
+
+def test():
+    pass
+
+def test1():
+    pass
+
+
+def amir1():
+    pass
+
+def amir2():
+    pass
