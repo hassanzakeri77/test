485e299d124957af2cad491fe85af4eaa1fdc027
Hassan_Zakeri <hassan.1377@gmail.com>
2021-01-31 00:17:13 +0330

    create new file

---
+++ python_auto_reset.py
@@ -0,0 +1,16 @@
+#! /bin/env python3
+import os
+import sys
+import time
+
+def like_cheese():
+    var = input("Hi! I like cheese! Do you like cheese?").lower()
+    if var == "yes":
+        print("That's awesome!")
+
+while True:
+    pass
+
+
+def test():
+    pass
